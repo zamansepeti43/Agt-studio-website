45c7f6c2196b6e1814312f028637829c10e75afc
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-07 00:22:32 +0300

    feat: route SEO service landing pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import AdminApp from './admin/AdminApp.tsx'
+import ServicePage from './components/ServicePage.tsx'
+
+const servicePaths = [
+  '/logo-tasarimi',
+  '/sosyal-medya-tasarimi',
+  '/web-tasarim',
+  '/menu-tasarimi',
+  '/kartvizit-tasarimi',
+  '/yapay-zeka-cozumleri',
+] as const
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/admin/*" element={<AdminApp />} />
-        <Route path="/*"       element={<App />} />
+        {servicePaths.map((path) => (
+          <Route key={path} path={path} element={<ServicePage />} />
+        ))}
+        <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
